af946f883f95b1c18ab9aec09cf92983d88cbd8b
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2020-05-27 20:37:32 -0300

    Array e Dicionario

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-var name = "Rafael Galleani";
-var idade = 29;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo";
+var frutas = [{nome: "maçã", cor:"vermelha"},{nome: "uva", cor:"roxo"}]
+console.log(frutas);
+alert(frutas[1].nome);
 
+/* var fruta = {nome: "maçã", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor); */
 
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var name = "Rafael Galleani";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
 //alert(name + " tem " + idade + " anos.");
 //alert(idade+idade2);
-console.log(name);
-console.log(idade+idade2);
-console.log(frase.toLowerCase());
+//console.log(name);
+//console.log(idade+idade2);
+//console.log(frase.toLowerCase());
 //alert(frase.replace("Japão","Brasil"));
