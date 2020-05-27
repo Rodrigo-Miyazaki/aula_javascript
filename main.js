3ba68bf16461352153dd4f06df562d5ac05b7e46
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2020-05-27 18:21:23 -0300

    Introdução ao Javascript

---
+++ main.js
@@ -0,0 +1,12 @@
+var name = "Rafael Galleani";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+
+//alert(name + " tem " + idade + " anos.");
+//alert(idade+idade2);
+console.log(name);
+console.log(idade+idade2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão","Brasil"));
